a495e057574880a5b417f8db44887a19955c1795
Manas Mittal <manasmittal.ce@gmail.com>
2025-02-05 01:41:31 +0530

    added typeConversion.js

---
+++ typeConversion.js
@@ -0,0 +1,15 @@
+let money = "50";
+//Convert string to number
+//money = parseInt(money)
+//money = +money
+//money = Number(money);
+
+//convert number to string
+let str = "50";
+//str = str.toString();
+//str = String(str);
+
+//Convert string to decimal
+let str2 = "50.3245";
+//str2 = parseFloat(str2);
+
