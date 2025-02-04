26b12614be358cd891935cd08309fbdab0a18f61
Manas Mittal <manasmittal.ce@gmail.com>
2025-02-05 01:11:11 +0530

    equality operators

---
+++ equality.js
@@ -0,0 +1,11 @@
+//Equality Operators
+// === strict equality (type+value)
+//!== strict non-equality operator (value+type)
+//== lose equality (value)
+//!= lose non equality (value)
+
+//by type checking we means datatype checking
+
+console.log(10 === 10); //true
+console.log(10 === "10"); //false
+console.log(10 == "10"); //true, as values are equal
