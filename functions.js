dc52fb0e4f0a1885e2d823bc12cdcfb766fbe69d
Manas Mittal <manasmittal.ce@gmail.com>
2025-02-05 10:29:18 +0530

    updated functions.js

--- functions.js
+++ functions.js
@@ -16,3 +16,28 @@ function func(x, y){
     return x+y;
 }
 console.log(func(10, 20));
+//function declaration and function expression
+
+//declaration
+function greeting(name){
+    console.log(`Hello to ${name}`)
+}
+
+//expression -> when we store a function declaration inside a variable and this type of function is anonymous function
+const hello = function(name){
+    console.log(`Hi ${name} iam Manas`);
+}
+hello("Mittal");
+
+//hoisting occurs incase of normal functions but incase function expressions no hoisting occurs you need to write function expression and then call it.
+
+//callback function
+//when a function is passed as an arguemnet to other function then that function is called as a callback function
+function fn(value){
+    console.log("Iam callback with value "+value);
+}
+function callfunc(fn){
+    const value = 10;
+    fn(value);
+}
+callfunc(fn);//donot call here just pass ref. of the callback function.
