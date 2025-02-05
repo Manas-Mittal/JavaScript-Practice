6d5eb30eb21fe1a66681b90634f2b31968e4e3df
Manas Mittal <manasmittal.ce@gmail.com>
2025-02-05 10:10:39 +0530

    Added functions.js

---
+++ functions.js
@@ -0,0 +1,12 @@
+//block of code performing specific task
+//function_name(parameter_list){.....}
+
+function greet(){
+    console.log("Hello");
+}
+greet();
+
+function sayHello(name){
+    console.log(`Hello to ${name}`)
+}
+sayHello("Manas");
