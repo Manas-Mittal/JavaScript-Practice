7a2a19021ac24d659456b9111bf3dbb19505a546
Manas Mittal <manasmittal.ce@gmail.com>
2025-02-05 18:54:04 +0530

    arrow_functions.js added

---
+++ arrow_functions.js
@@ -0,0 +1,5 @@
+//concise and short way of using functions.
+const greet = (params) => `Hello Manas`;
+//incase of one parameter then you can omit the params parenthesis.
+//and incase if your function has one line of code then we can omit curly braces and automatically that one line gets returned without writing return keyword.
+
