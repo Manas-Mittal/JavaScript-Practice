7e8ae875acf5e14b23b81ad53888f1a31876440d
Manas Mittal <manasmittal.ce@gmail.com>
2025-02-05 20:04:37 +0530

    updated arrow_functions.js

--- arrow_functions.js
+++ arrow_functions.js
@@ -6,3 +6,19 @@ const greet = (params) => `Hello Manas`;
 
 const calc = n => n*2;//this code takes one parameter 'n' and returns n*2 as this is onely one line function
 console.log(calc(5));
+
+setTimeout(()=>{
+    console.log("Hello");
+    setTimeout(()=>{
+        console.log("Hey");
+        setTimeout(()=>{
+            console.log("Namaste");
+            setTimeout(()=>{
+                console.log("Hi");
+                setTimeout(()=>{
+                    console.log("Bonjour");
+                }, 2000);
+            }, 2000);
+        }, 2000);
+    }, 2000);
+}, 2000);
