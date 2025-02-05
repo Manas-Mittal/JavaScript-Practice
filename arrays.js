b2b5ffb035ba423f2cd0e48460602ed99788a8ac
Manas Mittal <manasmittal.ce@gmail.com>
2025-02-05 09:10:28 +0530

    Added arrays.js

---
+++ arrays.js
@@ -0,0 +1,19 @@
+//let mylist = []; empty array
+let fruits = ["mango", "orange", "banana"];
+//we can store values of different datatypes in arrays.
+//nested arrays
+let mylist = ["one", "two", ["three", "four"], 1, true, false];
+//accessing nested array
+let insideArray = mylist[1];
+//Array methods
+//concat() -> join two or more arrays
+//includes() -> checks if an array contains a specified element
+//push() -> adds an element in the end of the array and returns the new length of the array
+//unshift() -> adds a new element in the start of the array and returns the new length of the array
+//pop() -> removes the last element of the array and returns the removed element
+//shift() -> removes the first element from the array and returns the removed element
+//sort() -> sort the elements alphabetically in strings and in ascending order
+//slice() -> selects a part of the array and returns the new array
+//splice() -> removes or replaces existing elements and/or adds new elements
+//join() -> joins the array into string based on specified separator
+//reverse() -> reverses the array
