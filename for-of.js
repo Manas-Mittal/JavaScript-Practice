710daa1aef4b35c931b3f48ad61fdb73afdc4d08
Manas Mittal <manasmittal.ce@gmail.com>
2025-02-10 22:08:15 +0530

    added for-of.js file

---
+++ for-of.js
@@ -0,0 +1,12 @@
+//for of loop iterates over the elemenets of the iterable like array or objects or maps, sets, stc without dealing with their keys or indices.
+
+let arr = [1, 2, 3, 4, 5];
+for(let variable of arr){
+    console.log(variable);
+}
+
+//on strings :
+const text = "Manas";
+for(const character of text){
+    console.log(character);
+} 
